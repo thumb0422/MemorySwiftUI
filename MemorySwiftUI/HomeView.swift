aa83615bdec2aa7269c92f257a00fc35b6130185
liuchunhua <thumb0422@163.com>
2020-06-13 23:02:59 +0800

    update
    update

---
+++ MemorySwiftUI/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  MemorySwiftUI
+//
+//  Created by liuchunhua on 2020/6/12.
+//  Copyright © 2020 liuchunhua. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView:View {
+    var body: some View{
+        Text("BBBBBBBBB").bold().foregroundColor(Color.yellow).italic()
+        
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ MemorySwiftUI/ListView.swift
@@ -0,0 +1,9 @@
+//
+//  ListView.swift
+//  MemorySwiftUI
+//
+//  Created by liuchunhua on 2020/6/13.
+//  Copyright © 2020 liuchunhua. All rights reserved.
+//
+
+import SwiftUI
